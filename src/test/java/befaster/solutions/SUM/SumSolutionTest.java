99eeeab4ffda13b38f905a24ea0f4e264f5aeca5
sbx_user1059 <sbx_user1059@ip-10-129-57-157.eu-west-2.compute.internal>
2018-09-15 20:11:52 +0000

    Sat Sep 15 20:11:52 UTC 2018

--- src/test/java/befaster/solutions/SUM/SumSolutionTest.java
+++ src/test/java/befaster/solutions/SUM/SumSolutionTest.java
@@ -1,7 +1,9 @@
 package befaster.solutions.SUM;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static junit.framework.TestCase.assertEquals;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -9,6 +11,9 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class SumSolutionTest {
     private SumSolution sum;
+
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
 
     @Before
     public void setUp() {
@@ -22,8 +27,15 @@ public class SumSolutionTest {
     }*/
 
     @Test
-    public void compute_sum() {
+    public void sum_1and0() {
         assertEquals(1, sum.compute(1, 0));
+    }
+
+    @Test
+    public void wrong_number_below_zero() {
+        thrown.expect(NumberFormatException.class);
+
+        sum.compute(-1, 0);
     }
 
 
