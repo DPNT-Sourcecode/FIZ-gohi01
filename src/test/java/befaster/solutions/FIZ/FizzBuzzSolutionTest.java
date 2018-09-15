fcbc609d576b85e1733b49fd909c725eb7b5f306
sbx_user1059 <sbx_user1059@ip-10-129-57-157.eu-west-2.compute.internal>
2018-09-15 20:50:18 +0000

    Sat Sep 15 20:50:18 UTC 2018

---
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -0,0 +1,12 @@
+package befaster.solutions.FIZ;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class FizzBuzzSolutionTest {
+    @Test
+    public void fizzBuzz() throws Exception {
+    }
+
+}
