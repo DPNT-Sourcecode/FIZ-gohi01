1ddeda163d6907b062027c890ddb1c91f33e5987
sbx_user1059 <sbx_user1059@ip-10-129-57-157.eu-west-2.compute.internal>
2018-09-15 20:56:18 +0000

    Sat Sep 15 20:56:18 UTC 2018

--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -1,12 +1,23 @@
 package befaster.solutions.FIZ;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class FizzBuzzSolutionTest {
+
+    private FizzBuzzSolution fizzBuzz;
+
+    @Before
+    public void setUp() {
+        fizzBuzz = new FizzBuzzSolution();
+    }
+
     @Test
-    public void fizzBuzz() throws Exception {
+    public void fizzBuzz() {
+        String answer = fizzBuzz.fizzBuzz(0);
+        assertEquals("0", answer);
     }
 
 }
